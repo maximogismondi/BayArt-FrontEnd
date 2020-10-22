5054646a817a46c2aa17384b6a500d832a436695
MaximoGismondi <63014012+MaximoGismondi@users.noreply.github.com>
2020-10-22 14:58:19 -0300

    Añado Python

---
+++ Python-BayArt!/CarrouselBayArt.py
@@ -0,0 +1,180 @@
+from tkinter import *
+from threading import Timer
+from PIL import ImageTk, Image
+import os
+
+#Ventana
+root = Tk()
+
+root.title("BayArt! - Carrousel")
+root.attributes("-fullscreen",TRUE)
+root.configure(bg="#242424")
+
+root.update()
+rootWidth = root.winfo_width()
+rootHeight = root.winfo_height()
+
+var = open("var.txt","r")
+
+rotateEnable = str(var.readline()[:-1])
+infoEnable = str(var.readline()[:-1])
+timeRotation = int(var.readline()[:-1])
+
+
+imageUrl = []
+index = 0
+actualUrl = var.readline()[:-1]
+
+while(actualUrl!=""):
+	imageUrl.append(actualUrl)
+	actualUrl = var.readline()[:-1]
+
+var.close()
+
+def resizeFullScreenImage(urlImage):
+        
+    image_no_resized = Image.open(urlImage)
+    width, height = image_no_resized.size
+    if(rootWidth / width < rootHeight / height):
+        image_resized = image_no_resized.resize((int(rootWidth*0.8),int((height/width*rootWidth)*0.8)),Image.ANTIALIAS)
+    else:
+        image_resized = image_no_resized.resize((int((width/height*rootHeight)*0.8),int(rootHeight*0.8)),Image.ANTIALIAS)
+
+    return image_resized
+
+def rotate():
+        global main_image
+        main_image = ImageTk.PhotoImage(resizeFullScreenImage(imageUrl[index]))
+        labelimagemain.configure(image=main_image)
+
+        if(rotateEnable == 'True'):
+                 timer = Timer(timeRotation,rotate_right).start()
+
+def rotate_left():
+        global index
+        if(index-1 >= 0):
+                index -= 1
+        else:
+                index = len(imageUrl)-1
+        rotate()
+
+def rotate_right():
+        global index
+        if(index+1 < len(imageUrl)):
+                index += 1
+        else:
+                index = 0
+        rotate()
+
+
+buttons_status = True
+def change_display_buttons():
+        global buttons_status
+        global label_left_arrow
+        global label_right_arrow
+        
+        if buttons_status:
+                label_left_arrow.place(relx = -1, rely = -1, anchor = CENTER)
+                label_right_arrow.place(relx = -1, rely = -1, anchor = CENTER)
+                hide_show_button['text'] = 'Show Buttons'
+        else:
+                label_left_arrow.place(relx = 0.03, rely = 0.5, anchor = CENTER)
+                label_right_arrow.place(relx = 0.97, rely = 0.5, anchor = CENTER)
+                hide_show_button['text'] = 'Hide Buttons'       
+        buttons_status  = not buttons_status 
+
+info_status = True
+def change_display_info():
+        global info_status
+        global info_container
+        global pictureNameLabel
+        global artistLabel
+        global button_close
+        
+        if info_status:
+                info_container.place(relx = -1, rely = -1, anchor = CENTER)
+                pictureNameLabel.place(relx = -1, rely = -1, anchor = CENTER)
+                artistLabel.place(relx = -1, rely = -1, anchor = CENTER)
+                button_close['bg'] = "#242424"
+                button_close['activebackground'] = "#242424"
+                hide_show_info['text'] = 'Show Info'
+        else:
+                info_container.place(relx = 0.5, rely = 0.05, anchor = CENTER)
+                pictureNameLabel.place(relx = 0.5, rely = 0.035, anchor = CENTER)
+                artistLabel.place(relx = 0.5, rely = 0.08, anchor = CENTER)
+                button_close['bg'] = "#3d3d3d"
+                button_close['activebackground'] = "#3d3d3d"
+                hide_show_info['text'] = 'Hide Info'
+        info_status  = not info_status 
+    
+main_image = ImageTk.PhotoImage(resizeFullScreenImage(imageUrl[index]))
+
+labelimagemain = Label(root, image=main_image, border=0, bg="#242424")
+labelimagemain.place(relx = 0.5, rely = 0.5, relwidth=1, relheight=1, anchor = CENTER)
+
+if rotateEnable == 'False':
+        #FlechaIzq
+        left_arrow_no_resized = Image.open("left_arrow.png")
+
+        left_arrow_resized = left_arrow_no_resized.resize((int(rootWidth/40),int(rootHeight/15)),Image.ANTIALIAS)
+
+        left_arrow = ImageTk.PhotoImage(left_arrow_resized)
+
+        label_left_arrow = Button(root ,image=left_arrow, command=rotate_left, border=0,activebackground="#242424", bg="#242424")
+        label_left_arrow.place(relx = 0.03, rely = 0.5, anchor = CENTER)
+        
+        #FlechaDer
+        right_arrow_no_resized = Image.open("right_arrow.png")
+
+        right_arrow_resized = right_arrow_no_resized.resize((int(rootWidth/40),int(rootHeight/15)),Image.ANTIALIAS)
+
+        right_arrow = ImageTk.PhotoImage(right_arrow_resized)
+
+        label_right_arrow = Button(root, image=right_arrow, command=rotate_right,activebackground="#242424", border=0, bg="#242424")
+        label_right_arrow.place(relx = 0.97, rely = 0.5, anchor = CENTER)
+
+        #Hide Show Buttons
+        hide_show_button = Button(root,text="Hide Buttons",font=("Verdana", 10, "bold"), command=change_display_buttons, border=0, bg="#00a797", fg="#242424" )
+        hide_show_button.place(relx = 0.95, rely = 0.98, anchor = CENTER)
+else:
+        #Loop
+        timer = Timer(timeRotation,rotate_right).start()
+
+if infoEnable == 'True':
+        info_container = Frame(root, bg="#3d3d3d", width=int(rootWidth), height=int(rootHeight/10))
+        info_container.place(relx = 0.5, rely = 0.05, anchor = CENTER)
+
+        artistLabel=Label(root,text="ArtistName", font=("Verdana", 12, "bold"), bg="#3d3d3d", fg="white")
+        artistLabel.place(relx = 0.5, rely = 0.08, anchor = CENTER)
+
+        pictureNameLabel=Label(root,text="PictureName", font=("Verdana", 30, "bold"), bg="#3d3d3d", fg="white")
+        pictureNameLabel.place(relx = 0.5, rely = 0.035, anchor = CENTER)
+
+        #Hide Show Info
+        hide_show_info = Button(root,text="Hide Info",font=("Verdana", 10, "bold"), command=change_display_info, border=0, bg="#00a797", fg="#242424" )
+        hide_show_info.place(relx = 0.04, rely = 0.98, anchor = CENTER)
+        
+
+#Boton cerrar
+        
+def openSettings():
+    root.destroy()
+    os.system('SettingsBayArt.py')
+        
+close_no_resized = Image.open("cancel.png")
+close_resized = close_no_resized.resize((int(rootWidth/27.2),int(rootHeight/15.36)),Image.ANTIALIAS)
+close_image = ImageTk.PhotoImage(close_resized)
+
+button_close = Button(root, command=openSettings, image=close_image,highlightthickness = 0, bd = 0)
+button_close.place(relx=0.98, rely=0.02,anchor=NE)
+
+if infoEnable == 'True':
+        button_close['bg'] = "#3d3d3d"
+        button_close['activebackground'] = "#3d3d3d"
+else:
+        button_close['bg'] = "#242424"
+        button_close['activebackground'] = "#242424"
+
+  
+root.mainloop()
+
---
+++ Python-BayArt!/LoginBayArt.py
@@ -0,0 +1,76 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import os
+
+#Ventana
+root = Tk()
+
+root.title("BayArt!")
+root.attributes("-fullscreen",TRUE)
+root.configure(bg="#242424")
+
+root.update()
+rootWidth = root.winfo_width()
+rootHeight = root.winfo_height()
+
+
+#Fondo
+background_no_resize = Image.open("background.jpg")
+background_resize = background_no_resize.resize((rootWidth+1,rootHeight+2),Image.ANTIALIAS)
+background_image = ImageTk.PhotoImage(background_resize)
+background = Label(image=background_image)
+background.place(x=0, y=0, relwidth=1, relheight=1)
+
+
+#Logo
+logoBayArt_no_resized = Image.open("logo.jpg")
+
+logoBayArt_resized = logoBayArt_no_resized.resize((int(rootWidth/3.4),int(rootHeight/5.12)),Image.ANTIALIAS)
+
+logoBayArt = ImageTk.PhotoImage(logoBayArt_resized)
+
+labelLogoBayArt = Label(root, image=logoBayArt, border=0)
+labelLogoBayArt.place(relx = 0.5, rely = 0.2, anchor = CENTER)
+
+#Login
+#Recuadro
+divLogin = Frame(root, bg="#3d3d3d", width=int(rootWidth/2.25), height=int(rootHeight/2.25))
+divLogin.place(relx = 0.5, rely = 0.6, anchor = CENTER)
+
+
+#Username
+usernameLabel=Label(root,text="Username", font=("Verdana", 20, "bold"), bg="#3d3d3d", fg="white")
+usernameEntry = Entry(root, width=int(rootWidth/44.21), font=("Verdana", 15, "bold"), bg="#7c7c7c", fg="white", bd=0)
+
+usernameLabel.place(relx = 0.37, rely = 0.45, anchor = CENTER)
+usernameEntry.place(relx = 0.50, rely = 0.51, anchor = CENTER)
+
+
+#Password
+passwordLabel=Label(root,text="Password", font=("Verdana", 20, "bold"), bg="#3d3d3d", fg="white")
+passwordEntry = Entry(root, width=int(rootWidth/44.21), font=("Verdana", 15, "bold"), show="*", bg="#7c7c7c", fg="white", bd=0)
+
+passwordLabel.place(relx = 0.37, rely = 0.57, anchor = CENTER)
+passwordEntry.place(relx = 0.50, rely = 0.63, anchor = CENTER)
+
+
+#LogIn
+def openSettings():
+    root.destroy()
+    os.system('SettingsBayArt.py')
+
+logInButton = Button(text="LogIn",font=("Verdana", 15, "bold"), command=openSettings ,width=int(rootWidth/113.33), height=2, bg="#00a797", fg="#242424" , activebackground="#674ea7", bd=0)
+logInButton.place(relx = 0.63, rely = 0.74, anchor = CENTER)
+
+
+#Boton cerrar
+close_no_resized = Image.open("cancel.png")
+close_resized = close_no_resized.resize((int(rootWidth/27.2),int(rootHeight/15.36)),Image.ANTIALIAS)
+close_image = ImageTk.PhotoImage(close_resized)
+
+button_close = Button(root, command=openSettings, image=close_image,highlightthickness = 0,bg="#242424", activebackground="#242424", bd = 0)
+button_close.place(relx=0.98, rely=0.02,anchor=NE)
+
+  
+#Loop
+root.mainloop()
